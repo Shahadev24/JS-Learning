0228efa869b182dd816820166be1dfeb4c7b0547
Shahadev24 <shahadev775@gmail.com>
2023-09-20 14:22:46 +0530

    Variables is JS
    swap the values program

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+var firstName
+firstName = "shahadev"; //variable declaration
+console.log(firstName); //Variable Initialization
+firstName = "mr.Shahadev";
+console.log(firstName);
+
+var lastName = "Warkhade";//declaration and initialization
+console.log(lastName);
+
+var height = "6"
+console.log(height);
+height = "6.2";
+console.log(height);
+var name;//bydefault value of variables is undefined..
+console.log(name);
+
