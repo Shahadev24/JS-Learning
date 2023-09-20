b645ad0cec3186840ce7ee94b5e6ab4cc1281055
Shahadev24 <shahadev775@gmail.com>
2023-09-20 14:22:19 +0530

    swap the values program
    swap the values program

---
+++ 01.swapVariable.js
@@ -0,0 +1,14 @@
+//write a program to swap the two values.
+var a;
+var b;
+console.log("===========Before swap============");
+a = "100";
+b = "150";
+console.log(a,b);
+
+console.log("===========After swap============");
+var temp = a;
+a = b;
+b = temp;
+console.log(a,b);
+
