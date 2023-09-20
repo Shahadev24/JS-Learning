7d2c88206d79de3cb72856cd57b9438df00a536f
Shahadev24 <shahadev775@gmail.com>
2023-09-20 14:21:42 +0530

    JS Assignment - 1.
    JS Assignment - 1.

---
+++ JS-Assignments/01.variableAssignment.js
@@ -0,0 +1,33 @@
+var myFullName;
+console.log(myFullName);
+myFullName = ("Shahadev Warkhade");
+console.log(myFullName);
+
+myFullName = ("shahadev Pandurang Warkhade");
+console.log(myFullName);
+
+var myLove = ("I love only JS");
+console.log(myLove);
+
+var firstName = ("Shahadev");
+console.log("First Name :", firstName);
+
+var lastName = ("Warkhade");
+console.log("Last Name :", lastName);
+
+var age = ("23");
+console.log("age :", age);
+
+var address = ("jalna");
+console.log("Address :", address);
+
+var pinCode = ("431209");
+console.log("Pin code :", pinCode);
+
+var gender = ("Male");
+console.log("Gender :", gender);
+
+var isMarried = ("Unmarried");
+console.log("Marital Status :", isMarried);
+
+
