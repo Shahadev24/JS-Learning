495b3383c8949ed0d8d0369c4855a9fcef6e2b6d
Shahadev24 <shahadev775@gmail.com>
2023-09-23 16:48:05 +0530

    Datatype and typeof  operator.
    Datatype and typeof  operator.

---
+++ JS-Assignments/02_typeOf.js
@@ -0,0 +1,21 @@
+console.log("*********Step-1********");
+var bankName;
+console.log("before initialization ==>", bankName, "data type ==>", typeof bankName);
+bankName = "SBI Bank";
+console.log("After initialization ==>", bankName, "data type ==>", typeof bankName);
+console.log("*********Step-2********");
+var marks = 90;
+console.log("Marks ==>", marks, "datatype ==>", typeof marks);
+console.log("*********Step-3********");
+var isWorking;
+console.log("before initialization ==>", isWorking, "data type ==>", typeof isWorking);
+var isWorking = true;
+console.log("After initialization ==>", isWorking, "data type ==>", typeof isWorking);
+console.log("*********Step-4********");
+var totalCount = "Hundred and Seven";
+console.log("Hundred and Seven", "datatype ==>", typeof totalCount);
+console.log("------After Update------");
+totalCount = 107;
+console.log("Updated value ==>", totalCount, "and it's Datatype is ==>", typeof totalCount);
+
+
