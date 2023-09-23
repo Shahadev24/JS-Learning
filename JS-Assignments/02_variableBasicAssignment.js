0e87e0bfda42bd6bd65bf753c231d6b9f55b245a
Shahadev24 <shahadev775@gmail.com>
2023-09-23 16:07:34 +0530

    Variable basic Assignment.
    Variable basic Assignment.

---
+++ JS-Assignments/02_variableBasicAssignment.js
@@ -0,0 +1,52 @@
+console.log("========== Assignment No - 1 ==========");
+
+console.log("---------------------------------------------------");
+
+console.log("==== Step First ====");
+
+console.log("===== Number datatype =====");
+var birthYear = 2000;
+var hscPercentage = 79;
+var sscPercentage = 83;
+
+console.log("birth Year =>", birthYear, "datatype =>", typeof birthYear);
+console.log("HSC percentage =>", hscPercentage, "type =>", typeof hscPercentage);
+console.log("SSC percentage =>", sscPercentage, "type =>", typeof sscPercentage);
+
+console.log("---------------------------------------------------");
+
+console.log('==== String Datatype ====');
+var firstName = "Shahadev";
+var lastName = "Warkhade";
+var collegeName = "Alard college of business studies";
+
+console.log("First Name =>", firstName, "type =>", typeof firstName);
+console.log("Last Name =>", lastName, "type =>", typeof lastName);
+console.log("College name =>", collegeName, "type =>", typeof collegeName);
+
+console.log("---------------------------------------------------");
+
+console.log("==== Boolean Datatype ====")
+var isMarried = false;
+var isHscPass = true;
+var isSscPass = true;
+
+console.log(isMarried, "type =>", typeof isMarried);
+console.log(isHscPass, "type =>", typeof isHscPass);
+console.log(isSscPass, "type =>", typeof isSscPass);
+
+console.log("---------------------------------------------------");
+
+console.log("===== Step 2 =====")
+
+console.log("---------------------------------------------------");
+
+console.log("------------- Difference between Undefined & Null Datatype ---------------");
+
+console.log("When we declare the 'VARIABLE' but not assign the 'VALUE', then output will be 'undefined'.");
+console.log("Without declaration of value of that variable byDefault is 'Undefined'.")
+
+console.log("----------------------------------------------------------------------");
+
+console.log("Null means value is assigned to variable but it set to be null");
+console.log("null like is none ");
