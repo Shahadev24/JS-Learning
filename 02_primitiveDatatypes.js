ca46bdcf895d36d2a67a1c1a69a0e8981e6ee4e2
Shahadev24 <shahadev775@gmail.com>
2023-09-23 16:06:49 +0530

    Primitive Datatypes
    Primitive Datatypes.

---
+++ 02_primitiveDatatypes.js
@@ -0,0 +1,26 @@
+
+
+var age = 23;
+console.log( age,typeof age);
+
+var price = 55.55;
+console.log(price, typeof price);
+
+var firstName = "shahadev";
+console.log(typeof firstName);
+
+var city = 'pune';
+console.log("city =>", city, "data type =>", typeof city);
+
+// whatever we write in double Quote abd single quote it considered as string.
+var hscMarks = "75";
+console.log(hscMarks, typeof hscMarks);
+
+var isMarried = false;
+console.log(typeof isMarried);
+var isHandicap = false;
+console.log(typeof(isHandicap));
+
+console.log(typeof "shahadev warkhade");
+console.log(typeof 65);
+console.log(typeof true);
